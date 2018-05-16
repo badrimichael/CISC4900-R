f0aeae7e2b072f387f1f5014d88cd1e76576412f
Michael Badri <badrimichael@gmail.com>
2018-05-15 20:01:19 -0400

    Adjusted comments and removed duplicate code.

--- frequency polygon.R
+++ frequency polygon.R
@@ -1,5 +1,5 @@
 # Project Title: Simulations of Reinforcement Learning in an Ecological Task
-# Name: Michael Badri, Brooklyn College Department of Computer and Information Science
+# Author: Michael Badri, Brooklyn College Department of Computer and Information Science
 # Supervisor: Dr. Stefano Ghirlanda, Brooklyn College Department of Psychology
 
 # The goal of this project is to simulate the ecological task of behavior chaining using reinforcement learning
@@ -14,170 +14,92 @@
 library(ggplot2)
 
 # Read the two output files into the environment and specify the reward value.
-output_0 = read.csv(file = 'sampleoutput/output-0percent.csv', head = TRUE, sep = ',')
-output_10 = read.csv(file = 'sampleoutput/output-10percent.csv', head = TRUE, sep = ',')
-reward_value = 8000
+output = read.csv(file = 'output.csv', head = TRUE, sep = ',')
+reward_value = 25000
 
 # Divide the output files into multiple dataframes, one per agent type.
-for (agent_type in levels(output_0$Agent.Type)) {
-  write.csv(subset(output_0, Agent.Type == agent_type),
-            paste(agent_type,'0', sep=" "),
+for (agent_type in levels(output$Agent.Type)) {
+  write.csv(subset(output, Agent.Type == agent_type),
+            paste(agent_type),
             row.names = FALSE)
 }
 
-for (agent_type in levels(output_10$Agent.Type)) {
-  write.csv(subset(output_10, Agent.Type == agent_type),
-            paste(agent_type,'10', sep=" "),
-            row.names = FALSE)
-}
 
-q_agents_0 = read.csv(file = 'Q-learning 0', head = TRUE, sep = ',')
-sarsa_agents_0 = read.csv(file = 'SARSA 0', head = TRUE, sep = ',')
-expected_sarsa_agents_0 = read.csv(file = 'Expected SARSA 0', head = TRUE, sep =',')
-qv_agents_0 = read.csv(file = 'QV-learning 0', head = TRUE, sep = ',')
-
-q_agents_10 = read.csv(file = 'Q-learning 10', head = TRUE, sep = ',')
-sarsa_agents_10 = read.csv(file = 'SARSA 10', head = TRUE, sep = ',')
-expected_sarsa_agents_10 = read.csv(file = 'Expected SARSA 10', head = TRUE, sep =',')
-qv_agents_10 = read.csv(file = 'QV-learning 10', head = TRUE, sep = ',')
+q_agents = read.csv(file = 'Q-learning', head = TRUE, sep = ',')
+sarsa_agents = read.csv(file = 'SARSA', head = TRUE, sep = ',')
+expected_sarsa_agents = read.csv(file = 'Expected SARSA', head = TRUE, sep =',')
+qv_agents = read.csv(file = 'QV-learning', head = TRUE, sep = ',')
 
 # Automatically determine the total number of agents and number of agents per dataframe.
-agents_per_list = max(q_agents_0$Agent)
-total_num_of_agents = max(qv_agents_0$Agent)
+agents_per_list = max(q_agents$Agent)
+total_num_of_agents = max(qv_agents$Agent)
 
 # Create a list of agents and the timestep they recieved reward_value.
 i = 1
-q_learning_0 <- c()
+q_learning <- c()
 for (i in 1:agents_per_list) {
-  sub = q_agents_0[q_agents_0$Agent == i, ]
+  sub = q_agents[q_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  q_learning_0 = c(q_learning_0, besttime)
+  q_learning = c(q_learning, besttime)
 }
 
-sarsa_0 <- c()
+sarsa <- c()
 for (i in (agents_per_list + 1):(2 * agents_per_list)) {
-  sub = sarsa_agents_0[sarsa_agents_0$Agent == i, ]
+  sub = sarsa_agents[sarsa_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  sarsa_0 = c(sarsa_0, besttime)
+  sarsa = c(sarsa, besttime)
 }
 
-expected_sarsa_0 <- c()
+expected_sarsa <- c()
 for (i in (2*agents_per_list+1):(total_num_of_agents-agents_per_list)) {
-  sub = expected_sarsa_agents_0[expected_sarsa_agents_0$Agent == i, ]
+  sub = expected_sarsa_agents[expected_sarsa_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  expected_sarsa_0 = c(expected_sarsa_0, besttime)
+  expected_sarsa = c(expected_sarsa, besttime)
 }
 
-
-qv_learning_0 <- c()
+qv_learning <- c()
 for (i in (total_num_of_agents-agents_per_list+1):total_num_of_agents) {
-  sub = qv_agents_0[qv_agents_0$Agent == i, ]
+  sub = qv_agents[qv_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  qv_learning_0 = c(qv_learning_0, besttime)
-}
-
-i = 1
-q_learning_10 <- c()
-for (i in 1:agents_per_list) {
-  sub = q_agents_10[q_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  q_learning_10 = c(q_learning_10, besttime)
-}
-
-sarsa_10 <- c()
-for (i in (agents_per_list + 1):(2 * agents_per_list)) {
-  sub = sarsa_agents_10[sarsa_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  sarsa_10 = c(sarsa_10, besttime)
-}
-
-expected_sarsa_10 <- c()
-for (i in (2*agents_per_list+1):(total_num_of_agents-agents_per_list)) {
-  sub = expected_sarsa_agents_10[expected_sarsa_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  expected_sarsa_10 = c(expected_sarsa_10, besttime)
-}
-
-qv_learning_10 <- c()
-
-for (i in (total_num_of_agents-agents_per_list+1):total_num_of_agents)  {
-  sub = qv_agents_10[qv_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  qv_learning_10 = c(qv_learning_10, besttime)
+  qv_learning = c(qv_learning, besttime)
 }
 
 # Combine the lists.
-qplot_0 <- c()
-qplot_0$group[1:agents_per_list] <- "q-learning"
-qplot_0$val <- q_learning_0
-q_0<- cbind(qplot_0$group,qplot_0$val)
+qplot <- c()
+qplot$group[1:agents_per_list] <- "q-learning"
+qplot$val <- q_learning
+q<- cbind(qplot$group,qplot$val)
 
-qvplot_0 <- c()
-qvplot_0$group[1:agents_per_list] <- "qv-learning"
-qvplot_0$val <- qv_learning_0
-qv_0<- cbind(qvplot_0$group,qvplot_0$val)
+qvplot <- c()
+qvplot$group[1:agents_per_list] <- "qv-learning"
+qvplot$val <- qv_learning
+qv<- cbind(qvplot$group,qvplot$val)
 
-esarsaplot_0 <- c()
-esarsaplot_0$group[1:agents_per_list] <- "expected_sarsa"
-esarsaplot_0$val <- expected_sarsa_0
-sar1_0<-cbind(esarsaplot_0$group,esarsaplot_0$val)
+esarsaplot <- c()
+esarsaplot$group[1:agents_per_list] <- "expected_sarsa"
+esarsaplot$val <- expected_sarsa
+sar1<-cbind(esarsaplot$group,esarsaplot$val)
 
-sarsaplot_0 <- c()
-sarsaplot_0$group[1:agents_per_list] <- "sarsa"
-sarsaplot_0$val <- sarsa_0
-sar2_0<- cbind(sarsaplot_0$group,sarsaplot_0$val)
+sarsaplot <- c()
+sarsaplot$group[1:agents_per_list] <- "sarsa"
+sarsaplot$val <- sarsa
+sar2<- cbind(sarsaplot$group,sarsaplot$val)
 
+ggdf<-rbind(sar1,sar2,q,qv)
+ggdf <- data.frame(ggdf)
+colnames(ggdf) <- c("type","time")
+ggdf$time<- as.numeric(as.character(ggdf$time))
 
-ggdf_0<-rbind(sar1_0,sar2_0,q_0,qv_0)
-ggdf_0 <- data.frame(ggdf_0)
-colnames(ggdf_0) <- c("type","time")
-ggdf_0$time<- as.numeric(as.character(ggdf_0$time))
+# Plot lists on a frequency polygon.
+ggplot(ggdf,aes(x=time,fill=as.factor(type), color=type)) +geom_freqpoly(bins=20) + theme_classic() + ggtitle(paste(reward_value,"total reward:", agents_per_list, "of Each Agent", sep = " "))
 
-
-qplot_10 <- c()
-qplot_10$group[1:agents_per_list] <- "q-learning"
-qplot_10$val <- q_learning_10
-q_10<- cbind(qplot_10$group,qplot_10$val)
-
-qvplot_10 <- c()
-qvplot_10$group[1:agents_per_list] <- "qv-learning"
-qvplot_10$val <- qv_learning_10
-qv_10<- cbind(qvplot_10$group,qvplot_10$val)
-
-esarsaplot_10 <- c()
-esarsaplot_10$group[1:agents_per_list] <- "expected_sarsa"
-esarsaplot_10$val <- expected_sarsa_10
-sar1_10<-cbind(esarsaplot_10$group,esarsaplot_10$val)
-
-sarsaplot_10 <- c()
-sarsaplot_10$group[1:agents_per_list] <- "sarsa"
-sarsaplot_10$val <- sarsa_10
-sar2_10<- cbind(sarsaplot_10$group,sarsaplot_10$val)
-
-ggdf_10<-rbind(sar1_10,sar2_10,q_10,qv_10)
-ggdf_10 <- data.frame(ggdf_10)
-colnames(ggdf_10) <- c("type","time")
-ggdf_10$time<- as.numeric(as.character(ggdf_10$time))
-
-# Plot both lists on a frequency polygon.
-ggplot(ggdf_0,aes(x=time,fill=as.factor(type), color=type)) +geom_freqpoly(bins=20) + theme_classic() + ggtitle(paste(reward_value,"total reward:", agents_per_list, "of Each Agent (0 percent)", sep = " "))
-
-ggplot(ggdf_10,aes(x=time,fill=as.factor(type), color=type)) + geom_freqpoly(bins=20) + theme_classic() + ggtitle(paste(reward_value,"total reward:", agents_per_list, "of Each Agent (10 percent)", sep = " "))
 
        
--- histogram.R
+++ histogram.R
@@ -1,5 +1,5 @@
 # Project Title: Simulations of Reinforcement Learning in an Ecological Task
-# Name: Michael Badri, Brooklyn College Department of Computer and Information Science
+# Author: Michael Badri, Brooklyn College Department of Computer and Information Science
 # Supervisor: Dr. Stefano Ghirlanda, Brooklyn College Department of Psychology
 
 # The goal of this project is to simulate the ecological task of behavior chaining using reinforcement learning
@@ -14,170 +14,91 @@
 library(ggplot2)
 
 # Read the two output files into the environment and specify the reward value.
-output_0 = read.csv(file = 'sampleoutput/output500-0percent.csv', head = TRUE, sep = ',')
-output_10 = read.csv(file = 'sampleoutput/output500-10percent.csv', head = TRUE, sep = ',')
+output = read.csv(file = 'output.csv', head = TRUE, sep = ',')
 reward_value = 25000
 
 # Divide the output files into multiple dataframes, one per agent type.
-for (agent_type in levels(output_0$Agent.Type)) {
-  write.csv(subset(output_0, Agent.Type == agent_type),
-            paste(agent_type,'0', sep=" "),
+for (agent_type in levels(output$Agent.Type)) {
+  write.csv(subset(output, Agent.Type == agent_type),
+            paste(agent_type),
             row.names = FALSE)
 }
 
-for (agent_type in levels(output_10$Agent.Type)) {
-  write.csv(subset(output_10, Agent.Type == agent_type),
-            paste(agent_type,'10', sep=" "),
-            row.names = FALSE)
-}
 
-q_agents_0 = read.csv(file = 'Q-learning 0', head = TRUE, sep = ',')
-sarsa_agents_0 = read.csv(file = 'SARSA 0', head = TRUE, sep = ',')
-expected_sarsa_agents_0 = read.csv(file = 'Expected SARSA 0', head = TRUE, sep =',')
-qv_agents_0 = read.csv(file = 'QV-learning 0', head = TRUE, sep = ',')
+q_agents = read.csv(file = 'Q-learning', head = TRUE, sep = ',')
+sarsa_agents = read.csv(file = 'SARSA', head = TRUE, sep = ',')
+expected_sarsa_agents = read.csv(file = 'Expected SARSA', head = TRUE, sep =',')
+qv_agents = read.csv(file = 'QV-learning', head = TRUE, sep = ',')
 
-q_agents_10 = read.csv(file = 'Q-learning 10', head = TRUE, sep = ',')
-sarsa_agents_10 = read.csv(file = 'SARSA 10', head = TRUE, sep = ',')
-expected_sarsa_agents_10 = read.csv(file = 'Expected SARSA 10', head = TRUE, sep =',')
-qv_agents_10 = read.csv(file = 'QV-learning 10', head = TRUE, sep = ',')
-
-agents_per_list = max(q_agents_0$Agent)
-total_num_of_agents = max(qv_agents_0$Agent)
+agents_per_list = max(q_agents$Agent)
+total_num_of_agents = max(qv_agents$Agent)
 
 # Create a list of agents and the timestep they recieved reward_value.
 i = 1
-q_learning_0 <- c()
+q_learning <- c()
 for (i in 1:agents_per_list) {
-  sub = q_agents_0[q_agents_0$Agent == i, ]
+  sub = q_agents[q_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  q_learning_0 = c(q_learning_0, besttime)
+  q_learning = c(q_learning, besttime)
 }
 
-sarsa_0 <- c()
+sarsa <- c()
 for (i in (agents_per_list + 1):(2 * agents_per_list)) {
-  sub = sarsa_agents_0[sarsa_agents_0$Agent == i, ]
+  sub = sarsa_agents[sarsa_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  sarsa_0 = c(sarsa_0, besttime)
+  sarsa = c(sarsa, besttime)
 }
 
-expected_sarsa_0 <- c()
+expected_sarsa <- c()
 for (i in (2*agents_per_list+1):(total_num_of_agents-agents_per_list)) {
-  sub = expected_sarsa_agents_0[expected_sarsa_agents_0$Agent == i, ]
+  sub = expected_sarsa_agents[expected_sarsa_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  expected_sarsa_0 = c(expected_sarsa_0, besttime)
+  expected_sarsa = c(expected_sarsa, besttime)
 }
 
-qv_learning_0 <- c()
+qv_learning <- c()
 for (i in (total_num_of_agents-agents_per_list+1):total_num_of_agents) {
-  sub = qv_agents_0[qv_agents_0$Agent == i, ]
+  sub = qv_agents[qv_agents$Agent == i, ]
   sub2 = sub[sub$Reward == reward_value, ]
   Times = sub2$Time[!is.na(sub2$Time)]
   besttime <- min(Times)
-  qv_learning_0 = c(qv_learning_0, besttime)
+  qv_learning = c(qv_learning, besttime)
 }
 
-i = 1
-q_learning_10 <- c()
-for (i in 1:agents_per_list) {
-  sub = q_agents_10[q_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  q_learning_10 = c(q_learning_10, besttime)
-}
-
-sarsa_10 <- c()
-for (i in (agents_per_list + 1):(2 * agents_per_list))  {
-  sub = sarsa_agents_10[sarsa_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  sarsa_10 = c(sarsa_10, besttime)
-}
-
-expected_sarsa_10 <- c()
-for (i in (2*agents_per_list+1):(total_num_of_agents-agents_per_list)) {
-  sub = expected_sarsa_agents_10[expected_sarsa_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  expected_sarsa_10 = c(expected_sarsa_10, besttime)
-}
-
-qv_learning_10 <- c()
-for (i in (total_num_of_agents-agents_per_list+1):total_num_of_agents) {
-  sub = qv_agents_10[qv_agents_10$Agent == i, ]
-  sub2 = sub[sub$Reward == reward_value, ]
-  Times = sub2$Time[!is.na(sub2$Time)]
-  besttime <- min(Times)
-  qv_learning_10 = c(qv_learning_10, besttime)
-}
 
 # Plot histograms.
-qplot(q_learning_0,
-      main = paste(reward_value,"total reward:", agents_per_list, "Q-Learning Agents (0 percent)", sep = " "),
+qplot(q_learning,
+      main = paste(reward_value,"total reward:", agents_per_list, "Q-Learning Agents", sep = " "),
       xlab = 'Time-step',
       ylab = 'Frequency',
       fill=I("white"),
       col=I("black"),
       bins = 20)
 
-qplot(sarsa_0,
-      main = paste(reward_value,"total reward:", agents_per_list, "SARSA Agents (0 percent)", sep = " "),
+qplot(sarsa,
+      main = paste(reward_value,"total reward:", agents_per_list, "SARSA Agents", sep = " "),
       xlab = 'Time-step',
       ylab = 'Frequency',
       fill=I("white"),
       col=I("black"),
       bins = 20)
 
-qplot(expected_sarsa_0,
-      main = paste(reward_value,"total reward:", agents_per_list, "Expected-SARSA Agents (0 percent)", sep = " "),
+qplot(expected_sarsa,
+      main = paste(reward_value,"total reward:", agents_per_list, "Expected-SARSA Agents", sep = " "),
       xlab = 'Time-step',
       ylab = 'Frequency',
       fill=I("white"),
       col=I("black"),
       bins = 20)
 
-qplot(qv_learning_0,
-      main = paste(reward_value,"total reward:", agents_per_list, "QV-Learning Agents (0 percent)", sep = " "),
-      xlab = 'Time-step',
-      ylab = 'Frequency',
-      fill=I("white"),
-      col=I("black"),
-      bins = 20)
-
-qplot(q_learning_10,
-      main = paste(reward_value,"total reward:", agents_per_list, "Q-Learning Agents (10 percent)", sep = " "),
-      xlab = 'Time-step',
-      ylab = 'Frequency',
-      fill=I("white"),
-      col=I("black"),
-      bins = 20)
-
-qplot(sarsa_10,
-      main = paste(reward_value,"total reward:", agents_per_list, "SARSA Agents (10 percent)", sep = " "),
-      xlab = 'Time-step',
-      ylab = 'Frequency',
-      fill=I("white"),
-      col=I("black"),
-      bins = 20)
-
-qplot(expected_sarsa_10,
-      main = paste(reward_value,"total reward:", agents_per_list, "Expected-SARSA Agents (10 percent)", sep = " "),
-      xlab = 'Time-step',
-      ylab = 'Frequency',
-      fill=I("white"),
-      col=I("black"),
-      bins = 20)
-
-qplot(qv_learning_10,
-      main = paste(reward_value,"total reward:", agents_per_list, "QV-Learning Agents (10 percent)", sep = " "),
+qplot(qv_learning,
+      main = paste(reward_value,"total reward:", agents_per_list, "QV-Learning Agents", sep = " "),
       xlab = 'Time-step',
       ylab = 'Frequency',
       fill=I("white"),
